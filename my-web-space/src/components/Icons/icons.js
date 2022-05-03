4aab9929d5023e2aa8d5dbf979e8f858757688d0
davidcode10 <davidedson2030@outlook.com>
2022-05-03 18:58:02 +0200

    icons added

--- my-web-space/src/components/Icons/icons.js
+++ my-web-space/src/components/Icons/icons.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import './icons.css'
+import { FaGithub, FaInstagram, FaRegEnvelope, FaLinkedinIn } from 'react-icons/fa';
+
 
 function icons() {
     return (
         <div>
             <ol>
-                <li>GitHub</li>
-                <li>Instagram</li>
-                <li>LinkedIn</li>
-                <li>Email</li>
+                <li>
+                    <a href='https://github.com/davidcode10' target='_blank'>
+                        <FaGithub />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.instagram.com/davidek_official/?hl=en' target='_blank'>
+                        <FaInstagram />
+                    </a>
+                </li>
+                <li>
+                    <a href='' target='_blank'>
+                        <FaRegEnvelope />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/david-edson/' target='_blank'>
+                        <FaLinkedinIn />
+                    </a>
+                </li>
             </ol>
         </div>
     )
